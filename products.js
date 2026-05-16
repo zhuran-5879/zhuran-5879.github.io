5dfd336ad8c2fdaa61cb7e7d3d7f9907d91e26d2
zhuran-5879 <212886226@qq.com>
2026-05-16 11:31:17 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    id: '01',
+    title: '4档路飞+艾斯+大和 三觉醒全满星',
+    desc: '航海王燃烧意志高练度账号，4档路飞、艾斯、大和全部觉醒满星，海量资源，上手即玩。竞技场无敌，剧情全通。',
+    price: 1288,
+    server: '安卓官方1区',
+    images: [
+      'https://placehold.co/700x420/1a1f2b/f0c060?text=账号01-主图',
+      'https://placehold.co/700x420/1e232b/d4942b?text=账号01-详情'
+    ]
+  }
+];
